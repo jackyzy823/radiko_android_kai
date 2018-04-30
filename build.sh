b16e7385c1d8d57d1dbda1e22fbcc676a146e992
jackyzy823 <jackyzy823@gmail.com>
2018-04-30 23:31:28 +0800

    update build script

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 MYDIR=`pwd`
 BUILDDIR=$MYDIR/build
 # BUILDDIR=`/tmp/radiko_build` #could be anywhere except git project dir.
-rm -rf .git  #if builddir in git project folder then delete .git folder
+#rm -rf .git  #if builddir in git project folder then delete .git folder
 wget -O src.apk `curl -s  https://apkpure.com/radiko-jp-for-android/jp.radiko.Player/download/77-APK  |grep -oP "(?<=id=\"iframe_download\" src=\").*?(?=\")"`
 wget https://bitbucket.org/iBotPeaches/apktool/downloads/apktool_2.3.2.jar
 java -jar apktool_2.3.2.jar d src.apk -f -o $BUILDDIR
