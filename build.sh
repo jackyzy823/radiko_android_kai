d51531d2b957d806a2becb592534ee11a2fb54ad
jackyzy823 <jackyzy823@gmail.com>
2018-05-25 23:57:22 +0800

    update so patch according to radiko's updating

--- build.sh
+++ build.sh
@@ -1,13 +1,22 @@
+VERSIONCODE="78"
+VERSIONNAME="6.3.7"
 CURDIR=`pwd`
 MYDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 echo "Script is in the" $MYDIR
 BUILDTOOLDIR=$MYDIR/tool
-
+#
+# 77 -> 6.3.6 5331083aef76176cc668b181b87b750d
+# 78 -> 6.3.7 93fd37e15699ec94fd659e778b5a0ea4
+#
+#
+# if stable use latest valid version
+# else check update first if update available :  use parser to generate so.patch and use previous version smali.patch if specified file not changed.
+#
 downloadRadiko(){
     echo "Downloading radiko original apk"
-    DOWNLOADLINK=`curl -s  https://apkpure.com/radiko-jp-for-android/jp.radiko.Player/download/77-APK  |grep -oP "(?<=id=\"iframe_download\" src=\").*?(?=\")"`
+    DOWNLOADLINK=`curl -s  https://apkpure.com/radiko-jp-for-android/jp.radiko.Player/download/$VERSIONCODE-APK  |grep -oP "(?<=id=\"iframe_download\" src=\").*?(?=\")"`
     if [ $DOWNLOADLINK ];then
-        wget -O $BUILDTOOLDIR/src.apk  $DOWNLOADLINK || exit 1
+        wget -O $BUILDTOOLDIR/src-v$VERSIONNAME.apk  $DOWNLOADLINK || exit 1
     else
         exit 1
     fi
@@ -25,10 +34,10 @@ if [ ! -x "$BUILDTOOLDIR" ];then
     downloadRadiko
     downloadApktool
 else
-    if [ ! -a $BUILDTOOLDIR/src.apk -o `md5sum $BUILDTOOLDIR/src.apk |awk '{print $1}'` != '5331083aef76176cc668b181b87b750d' ] ;then
+    if [ ! -f $BUILDTOOLDIR/src-v$VERSIONNAME.apk -o `md5sum $BUILDTOOLDIR/src-v$VERSIONNAME.apk |awk '{print $1}'` != '93fd37e15699ec94fd659e778b5a0ea4' ] ;then
         downloadRadiko
     fi
-    if [ ! -a $BUILDTOOLDIR/apktool_2.3.2.jar -o `md5sum $BUILDTOOLDIR/apktool_2.3.2.jar |awk '{print $1}'` !=  '953ed8a553becac4e713d1073912f15f' ];then
+    if [ ! -f $BUILDTOOLDIR/apktool_2.3.2.jar -o `md5sum $BUILDTOOLDIR/apktool_2.3.2.jar |awk '{print $1}'` !=  '953ed8a553becac4e713d1073912f15f' ];then
         downloadApktool
     fi
 fi
@@ -36,15 +45,20 @@ fi
 BUILDDIR=`mktemp -d`
 echo "Create tmp build directory" $BUILDDIR
 echo "Decompiling"
-java -jar $BUILDTOOLDIR/apktool_2.3.2.jar d $BUILDTOOLDIR/src.apk -f -o $BUILDDIR
-echo "Applying patch from " $MYDIR/v6.3.6.patch
+java -jar $BUILDTOOLDIR/apktool_2.3.2.jar d $BUILDTOOLDIR/src-v$VERSIONNAME.apk -f -o $BUILDDIR
 cd $BUILDDIR 
-git apply --stat $MYDIR/v6.3.6.patch
-git apply $MYDIR/v6.3.6.patch
+echo "Applying patch from " $MYDIR/patch/$VERSIONNAME/smali.patch
+git apply --stat $MYDIR/patch/$VERSIONNAME/smali.patch
+git apply $MYDIR/patch/$VERSIONNAME/smali.patch
+#if debug , generate so.patch from parser.py
+#
+echo "Applying patch from " $MYDIR/patch/$VERSIONNAME/so.patch
+git apply --stat $MYDIR/patch/$VERSIONNAME/so.patch
+git apply $MYDIR/patch/$VERSIONNAME/so.patch
 echo "Compiling && Packaging && Signing"
 java -jar $BUILDTOOLDIR/apktool_2.3.2.jar b $BUILDDIR
-cd $BUILDDIR/dist && keytool -genkey -keystore fake.jks -keyalg RSA -keysize 2048 -alias fake  -storepass 123456 -dname "CN=fake" -keypass 123456  -validity 36500 && jarsigner -keystore fake.jks -digestalg SHA1 -sigalg SHA1withRSA -storepass 123456 -signedjar $MYDIR/radiko_kai_6.3.6.apk src.apk fake 
+cd $BUILDDIR/dist && keytool -genkey -keystore fake.jks -keyalg RSA -keysize 2048 -alias fake  -storepass 123456 -dname "CN=fake" -keypass 123456  -validity 36500 && jarsigner -keystore fake.jks -digestalg SHA1 -sigalg SHA1withRSA -storepass 123456 -signedjar $MYDIR/radiko_kai_$VERSIONNAME.apk src-v$VERSIONNAME.apk fake 
 echo "Removing tmp directory" $BUILDDIR
 rm -r $BUILDDIR
-echo "Result is " $MYDIR/radiko_kai_6.3.6.apk
+echo "Result is " $MYDIR/radiko_kai_$VERSIONNAME.apk
 cd $CURDIR
